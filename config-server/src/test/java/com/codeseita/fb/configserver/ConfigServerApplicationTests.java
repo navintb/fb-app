4015330415e1fc48d1aea2f95798db1dc5eef532
navin <navintb@gmail.com>
2019-12-18 00:06:18 +0530

    Eureka and config

---
+++ config-server/src/test/java/com/codeseita/fb/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeseita.fb.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ eureka-server/src/test/java/com/codeseita/fb/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeseita.fb.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
